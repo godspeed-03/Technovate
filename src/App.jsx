7dbed383524ec29f1d1b3ccf82b439edfb4387e3
kashnx <kashnii250@gmail.com>
2024-03-06 10:12:07 +0530

    Join Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Landingpage from './Pages/Landingpage';
-import JoinForm from './Pages/JoinForm';
-import JoinExpect from './Pages/JoinExpect';
-
+import Landingpage from "./Pages/Landingpage";
+import JoinForm from "./Pages/JoinForm";
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route exact path="/" element={<JoinForm/>} />
+          <Route exact path="/" element={<Landingpage />} />
+          <Route exact path="/form" element={<JoinForm />} />
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
