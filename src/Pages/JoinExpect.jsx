d46cfe1de9ddaa2d562db059022101e071bd0c57
krishna jain <krishnajain5050@gmail.com>
2024-03-09 23:28:23 +0530

    refrefshment notice added

--- src/Pages/JoinExpect.jsx
+++ src/Pages/JoinExpect.jsx
@@ -169,6 +169,25 @@ function JoinExpect() {
                       </span>
                     </div>
                   </div>
+                  <div className="w-full p-2">
+                    <div className="flex h-full rounded ">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="3"
+                        class="text-indigo-500 w-6 h-6 flex-shrink-0 mr-4"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+                        <path d="M22 4L12 14.01l-3-3"></path>
+                      </svg>
+                      <span class="font-content  max-md:text-sm ">
+                        NOTE: Refreshments will not be provided, However you can purchase food coupons on the day of Hackathon
+                      </span>
+                    </div>
+                  </div>
                 </div>
 
 
